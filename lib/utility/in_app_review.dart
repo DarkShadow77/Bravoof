17b5cbe7c1e218d9564f9d3204a94dda574d1375
David Adeshina <49479389+DarkShadow77@users.noreply.github.com>
2026-03-05 17:45:56 +0100

    chore: Bump app version to 1.0.69+70
    This commit increments the application version from `1.0.68+69` to `1.0.69+70` in `pubspec.yaml`.

--- lib/utility/in_app_review.dart
+++ lib/utility/in_app_review.dart
@@ -3,14 +3,13 @@ import 'package:in_app_review/in_app_review.dart';
 Future<void> requestAppRating() async {
   final inAppReview = InAppReview.instance;
 
-  if (await inAppReview.isAvailable()) {
-    await inAppReview.requestReview();
-
-    // Optional fallback
-    Future.delayed(const Duration(seconds: 1), () {
-      inAppReview.openStoreListing();
-    });
-  } else {
-    await inAppReview.openStoreListing();
+  try {
+    if (await inAppReview.isAvailable()) {
+      await inAppReview.requestReview();
+    } else {
+      await inAppReview.openStoreListing(appStoreId: '6756618013');
+    }
+  } catch (e) {
+    await inAppReview.openStoreListing(appStoreId: '6756618013');
   }
 }
